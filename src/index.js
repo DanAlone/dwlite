f8deeef958fc31c7d6d875979f47a89ae1e536ea
Dan <168823515+DanAlone@users.noreply.github.com>
2024-05-16 17:32:24 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import './dwlite.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import 'semantic-ui-css/semantic.min.css';
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'core-js/stable';
+
+import Login from './Login.js';
+import Main from './Main.js';
+
+const root = createRoot(document.getElementById("root"));
+
+root.render (
+  //<React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" default exact element={<App />} />
+      <Route path="/home" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  //</React.StrictMode>,
+  //document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
